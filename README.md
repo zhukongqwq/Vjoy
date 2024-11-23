2dc4d2a67027739a6ca71d9f553a55d90d2c5a12
zhukong <1583393149@qq.com>
2024-11-23 11:26:23 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Vjoy
-电脑按键操控云游戏触控摇杆
+因为手机玩游戏太卡所以用网易云游戏玩的时候发现不支持摇杆所以自己写了这个虚拟摇杆
+
+# 使用
+
+### 1.安装python（建议3.10及以上）
+
+### 2.用git下载源码
+
+```git
+git clone https://github.com/zhukongqwq/Vjoy.git
+```
+
+### 3.pip安装所需库
+
+```python
+pip install -r r.txt
+```
+
+### 4.首先运行set_opos.py，获取摇杆中心坐标
+
+- 打开云游戏，进入有摇杆的游戏界面（建议全屏显示）
+
+- 将鼠标放在摇杆中心位置，用Alt+Tab快捷键切换到运行python文件的窗口（如cmd，python编辑器）并使用快捷键运行set_opos.py，选择你的云游戏窗口，**在此过程中不要移动鼠标**
+
+  ![untitled](untitled.png)
+
+### 5.运行vjoy.py
+
+- 打开vjoy.py，用控制台输出的坐标替换o_pos的数据
+- 运行vjoy.py，选择云游戏窗口
+- 回到游戏，就可以用键盘操控摇杆了
+
+由于作者水平有限，代码运行不是很流畅，可能存在一些bug，欢迎提出issue
---
+++ vjoy.py
@@ -0,0 +1,68 @@
+import keyboard as k
+import win32api,win32gui,win32con
+
+status = 0
+o_pos = (285, 630)
+w_pos = (o_pos[0], o_pos[1]-50)
+s_pos = (o_pos[0], o_pos[1]+50)
+a_pos = (o_pos[0]-50, o_pos[1])
+d_pos = (o_pos[0]+50, o_pos[1])
+hwnds = []
+
+def callback(hwnd, extra):
+    global hwnds
+    if win32gui.IsWindowVisible(hwnd):
+        hwnds.append(hwnd)
+        title = win32gui.GetWindowText(hwnd)
+        print("{} 窗口标题:".format(len(hwnds)), title, " 窗口句柄:", hwnd)
+    return True
+
+
+win32gui.EnumWindows(callback, None)
+hwnd = hwnds[int(input("输入你需要的窗口编号："))-1]
+ 
+'''根据GetWindowRect拿到主窗口的左顶点的位置坐标(x,y)和窗口的宽高(w*h)'''
+rect = win32gui.GetWindowRect(hwnd)
+print(rect)
+x,y=rect[0],rect[1]
+w,h=rect[2] - x,rect[3] - y
+long_position = win32api.MAKELONG(x, y)
+
+def down(x,y):
+    global status, long_position
+    if status == 0:
+        long_position = win32api.MAKELONG(o_pos[0],o_pos[1])
+        win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, long_position)
+        long_position = win32api.MAKELONG(x, y)
+        win32api.PostMessage(hwnd, win32con.WM_MOUSEMOVE, None, long_position)
+        status = 1
+
+def up():
+    global status, long_position
+    status = 0
+    win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, long_position)
+
+while True:
+    # 等待下一个事件。
+    event = k.read_event()
+    if event.event_type == k.KEY_DOWN:
+        if k.is_pressed("w") and k.is_pressed("d"):
+            down(d_pos[0],w_pos[1])
+        elif k.is_pressed("w") and k.is_pressed("a"):
+            down(a_pos[0],w_pos[1])
+        elif k.is_pressed("s") and k.is_pressed("d"):
+            down(d_pos[0],s_pos[1])
+        elif k.is_pressed("s") and k.is_pressed("a"):
+            down(a_pos[0],s_pos[1])
+        else:
+            if k.is_pressed("w"):
+                down(w_pos[0],w_pos[1])
+            elif k.is_pressed("s"):
+                down(s_pos[0],s_pos[1])
+            elif k.is_pressed("a"):
+                down(a_pos[0],a_pos[1])
+            elif k.is_pressed("d"):
+                down(d_pos[0],d_pos[1])
+        status = 0
+    elif event.event_type == k.KEY_UP:
+        up()
